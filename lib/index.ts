10effda736690aca47d3d9e4b70aab26647c5d22
nicolasgere <nicolas.gerelm@gmail.com>
2020-04-19 14:25:28 -0400

    first commit

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,10 @@
 
-import io from 'socket.io-client';
+import * as io from 'socket.io-client';
 
 class Buzz {
+   private handlers:any;
+   private presences:any; 
+   private socket:any;
    constructor() {
        this.handlers = {};
        this.presences = {};
@@ -18,30 +21,30 @@ class Buzz {
            }
        })
        this.socket.on('disconnect', function () {
-           io.socket.removeAllListeners()
+           io.Socket.removeAllListeners()
        });
-       this.socket.on('entry-message', (data) => {   
+       this.socket.on('entry-message', (data:any) => {   
            var parsed = JSON.parse(atob(data))
            if(this.handlers[parsed.channel + parsed.topic]){
-               this.handlers[parsed.channel + parsed.topic].forEach((handler)=>{
+               this.handlers[parsed.channel + parsed.topic].forEach((handler:any)=>{
                    handler.callback(JSON.parse(parsed.data))
                })
            }
        })
-       this.socket.on('entry-presence', (data) => {   
+       this.socket.on('entry-presence', (data:any) => {   
            var parsed = JSON.parse(atob(data))
-           var key;
-           var values = parsed.map((d)=>{
+           var key = "";
+           var values = parsed.map((d:any)=>{
                var message = JSON.parse(d)
                key = message.channel + message.topic
                return JSON.parse(message.data)
            })
-           this.presences[key].forEach((h)=>{
+           this.presences[key].forEach((h:any)=>{
                h.callback(values)
            })
        })
    }
-   subscribe = (channel, topic, callback) => {
+   subscribe = (channel:any, topic:any, callback:any) => {
        this.socket.emit('subscribe', { channel, topic })
        if (this.handlers[channel + topic] === undefined) {
            this.handlers[channel + topic] = [{ channel, topic, callback }]
@@ -49,15 +52,7 @@ class Buzz {
            this.handlers[channel + topic].push({ channel, topic, callback })
        }
    }
-   subscribe = (channel, topic, callback) => {
-       this.socket.emit('subscribe', { channel, topic })
-       if (this.handlers[channel + topic] === undefined) {
-           this.handlers[channel + topic] = [{ channel, topic, callback }]
-       } else {
-           this.handlers[channel + topic].push({ channel, topic, callback })
-       }
-   }
-   presenceSubscribe = (channel, topic, callback) => {
+   presenceSubscribe = (channel:any, topic:any, callback:any) => {
        setInterval(() => {
            this.socket.emit('presence', { channel, topic })
        }, 10000);
@@ -68,14 +63,14 @@ class Buzz {
            this.presences[channel + topic].push({ channel, topic, callback })
        }
    }
-   presence = (channel, topic, key, data) => {
+   presence = (channel:any, topic:any, key:any, data:any) => {
        setInterval(() => {
            this.socket.emit('heartbeat', { channel, topic, key, data: JSON.stringify(data) })
        }, 10000);
        this.socket.emit('heartbeat', { channel, topic, key, data: JSON.stringify(data) })
    }
 
-   send = (channel, topic, data) => {
+   send = (channel:any, topic:any, data:any) => {
        console.log('send-message')
        this.socket.send({ channel, topic, data: JSON.stringify(data) })
    }
